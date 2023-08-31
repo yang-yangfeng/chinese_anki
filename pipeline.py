effe13d0e3f46c7c83b9853c8987df34d92e9367
Yang-Yang Feng <yangyang.wusm@gmail.com>
2023-08-31 13:25:57 -0500

    first

---
+++ pipeline.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# vocab HSK anki
+# pipeline to make my own hsk anki cards
+
+# create sql db of all HSK words with hanzi, pinyin, chinese from this: https://github.com/bachhuberdesign/chinese-vocabulary-database/blob/master/hsk-wordlist.sql
+# DONE
+
+# load db
+
+# for each row in db
+# get audio from google translate https://github.com/hungtruong/Google-Translate-TTS/blob/master/GoogleTTS.py
+
+# write csv for anki import https://docs.ankiweb.net/importing/text-files.html, https://docs.ankiweb.net/importing/text-files.html#importing-media, https://superuser.com/questions/698902/can-i-create-an-anki-deck-from-a-csv-file
+
+# import to anki
